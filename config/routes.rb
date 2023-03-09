956eb63fa32216735bfd595b5165d9aa83c3ce50
Vinleon Ang <javadripdev@gmail.com>
2023-03-09 17:41:15 +0800

    Add bookmarks controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: %i[index show new create]
   # Defines the root path route ("/")
