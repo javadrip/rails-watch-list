a160e2bd491501f7f0d06c0d0589064e4bd644f9
Vinleon Ang <javadripdev@gmail.com>
2023-03-09 17:55:03 +0800

    Add routes for bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
